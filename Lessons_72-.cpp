3f41ec4871c1eac8e5489ed14246b4d0bbdece34
OlegVorontsov <oleg-voroncov@mail.ru>
2022-04-24 10:15:44 +0300

    Add project files.

---
+++ Lessons_72-.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Human
+{
+private:
+	string name;
+	int age;
+	int weight;
+
+	bool Check()
+	{
+		return true;
+	}
+
+public:
+	void Setname(string valuename)
+	{
+		name = valuename;
+	}
+	void Setage(int valueage)
+	{
+		age = valueage;
+	}
+	void Setweight(int valueweight)
+	{
+		weight = valueweight;
+	}
+	int Getage()
+	{
+		return age++;
+	}
+	void Print()
+	{
+		if (Check())
+		{
+			cout << "Имя: " << name << "\nВозраст: " << age << "\nВес: " << weight << endl;
+		}
+		else
+		{
+			cout << "В доступе к данным отказано!" << endl;
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	Human FirstMan;
+	FirstMan.Setname("Иван");
+	FirstMan.Setage(40);
+	FirstMan.Setweight(80);
+	FirstMan.Getage();
+	FirstMan.Print();
+
+	return 0;
+}
